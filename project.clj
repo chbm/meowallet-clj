b3426390c0582aeda383dbad71973a0dad75a991
Carlos Morgado <chbm@primatas.org>
2015-01-07 00:17:53 +0000

    * project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject meowallet-clj "1.0.0"
   :description "MEO Wallet library"
-  :url "notyet"
+  :url "https://github.com/chbm/meowallet-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
